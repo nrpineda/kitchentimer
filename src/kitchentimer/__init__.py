fd596b61ec5ac4d2dc48484411fb68b83bb9a1bb
nrpineda <new.lifenrpn@gmail.com>
2026-08-28 19:18:44 -0700

    refactor screen size

--- src/kitchentimer/__init__.py
+++ src/kitchentimer/__init__.py
@@ -1,9 +1,11 @@
 # Example file showing a circle moving on screen
 import pygame
+SCREEN_WIDTH=1280
+SCREEN_HEIGHT=720
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
