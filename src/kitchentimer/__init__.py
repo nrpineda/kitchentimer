9b91b165406366c438176d54b5ae4645348bcd36
nrpineda <new.lifenrpn@gmail.com>
2026-08-28 20:43:12 -0700

    deleted old code

--- src/kitchentimer/__init__.py
+++ src/kitchentimer/__init__.py
@@ -1,4 +1,3 @@
-# Example file showing a circle moving on screen
 import pygame
 SCREEN_WIDTH=1280
 SCREEN_HEIGHT=720
@@ -11,8 +10,6 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -23,8 +20,6 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    #pygame.draw.circle(screen, "red", player_pos, 40)
-
     # pick a font you have and set its size
     myfont = pygame.font.SysFont("Helvetica", 30)
     # apply it to text on a label
@@ -34,15 +29,7 @@ while running:
     # put the label object on the screen at point x=100, y=100
     screen.blit(label, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
-    keys = pygame.key.get_pressed()
-   # if keys[pygame.K_w]:
     value = value + dt
-    # if keys[pygame.K_s]:
-    #     #value = "0001"
-    # if keys[pygame.K_a]:
-    #    # value = "0002"
-    # if keys[pygame.K_d]:
-    # value = "0003"
 
     # flip() the display to put your work on screen
     pygame.display.flip()
